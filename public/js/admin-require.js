c3c27dbd778ff4536e2bb6efbdd27eae6dfe3332
Emil Hemdal <emil.hemdal.em@gmail.com>
2015-12-28 14:10:51 +0100

    Initial React user management.

--- public/js/admin-require.js
+++ public/js/admin-require.js
@@ -28,7 +28,80 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
 
+var $ = require('jquery');
+
+var UserManagement = React.createClass({
+  loadUsersFromServer: function() {
+    $.ajax({
+      url: this.props.url/*'/api/users'*/,
+      type: 'GET',
+      dataType: 'json',
+      cache: false,
+      success: function(data) {
+        this.setState({data: data});
+      }.bind(this),
+      error: function(xhr, status, err) {
+        // TODO: error handling
+        console.error(this.props.url, status, err.toString());
+      }.bind(this)
+    });
+  },
+  getInitialState: function() {
+    return {data: []};
+  },
+  componentDidMount: function() {
+    this.loadUsersFromServer();
+  },
+  render: function() {
+    return (
+      <div className="userManagement">
+        <h1>Users</h1>
+        <UserList data={this.state.data} />
+      </div>
+    );
+  }
+});
+
+var UserList = React.createClass({
+  render: function() {
+    var userNodes = this.props.data.map(function(user) {
+      return (
+        <User fullName={user.firstName + ' ' + user.lastName} id={user.id} />
+      );
+    });
+    return (
+      <div className="userList">
+        {userNodes}
+      </div>
+    );
+  }
+});
+
+var UserForm = React.createClass({
+  render: function() {
+    return (
+      <form className="userForm">
+        <input type="text" placeholder="Förnamn" />
+        <input type="text" placeholder="Efternamn" />
+        <input type="submit" value="Post" />
+      </form>
+    );
+  }
+});
+
+var User = React.createClass({
+  render: function() {
+    return (
+      <div className="user" key={this.props.id}>
+        <div className="userFullName">
+          {this.props.fullName}
+        </div>
+      </div>
+    );
+  }
+});
+
 ReactDOM.render(
-  <h1>Hello, world!</h1>,
-  document.getElementById('example')
+  <UserManagement url="/data.json" />,
+  document.getElementById('content')
 );
